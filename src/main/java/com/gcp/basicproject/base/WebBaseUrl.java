597cf1693b59f0798cf6de4c3c89649352fb73ae
郭长平 <2046039989@qq.com>
2022-03-31 09:43:12 +0800

    feat:增加基础url

---
+++ src/main/java/com/gcp/basicproject/base/WebBaseUrl.java
@@ -0,0 +1,16 @@
+package com.gcp.basicproject.base;
+
+/**
+ * @author Admin
+ */
+public class WebBaseUrl {
+
+    private static final String ADMIN_URL = "web-admin";
+
+    private static final String RECEPTION_URL = "web-reception";
+
+    public WebBaseUrl(){
+
+    }
+
+}
